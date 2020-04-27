8dcdc57b15d79a7fc849f69c56c9802651940a4d
Lai chane an <m020203@gms.tku.edu.tw>
2020-04-27 15:48:29 +0800

    fix: fix knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,13 @@
 // Update with your config settings.
+require('ts-node/register');
+
+const config = require('./config').default;
 
 module.exports = {
 
   development: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DB_NAME,
-      port: parseInt(process.env.PG_PORT)
-    },
+    connection: config.postgres,
     pool: {
       min: 2,
       max: 10
@@ -26,13 +23,7 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      host: process.env.PG_HOST,
-      user: process.env.PG_USER,
-      password: process.env.PG_PASSWORD,
-      database: process.env.PG_DB_NAME,
-      port: parseInt(process.env.PG_PORT)
-    },
+    connection: config.postgres,
     pool: {
       min: 2,
       max: 10
