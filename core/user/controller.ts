006d0ddcdcddd813566cd823d563551f07794e4d
Lai chane an <m020203@gms.tku.edu.tw>
2020-05-13 02:55:52 +0800

    feat: add user's name and email checker

--- core/user/controller.ts
+++ core/user/controller.ts
@@ -3,6 +3,8 @@ import {insertUser, selectUserByParam} from "./model";
 import {ResponseFormat} from "../error/type";
 import {User} from "./type";
 import * as utils from '../../utils';
+import _ from 'lodash';
+import BadRequestError from "../error/BadRequestError";
 
 export const userRegister = async (ctx: Context) => {
   const {
@@ -11,6 +13,25 @@ export const userRegister = async (ctx: Context) => {
     password
   } = ctx.request.body;
 
+  //check is user name already been used
+  const isUserNameDuplicate: boolean = !_.isEmpty(await selectUserByParam({
+    name: name,
+    deletedAt: null
+  }));
+  if (isUserNameDuplicate) {
+    throw new BadRequestError(`User's name already been used`, 400);
+  }
+
+  //check is email name already been used
+  const isUserEmailDuplicate: boolean = !_.isEmpty(await selectUserByParam({
+    email: email,
+    deletedAt: null
+  }));
+  if (isUserEmailDuplicate) {
+    throw new BadRequestError(`User's email already been used`, 400);
+  }
+
+  //create user
   const insertedUserId: string = await insertUser({
     id: utils.generateUUID(),
     name,
